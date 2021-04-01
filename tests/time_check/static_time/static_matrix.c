a787c2922ba0ef8ededea672d883d64ba34402f2
fillinmar <72233158+fillinmar@users.noreply.github.com>
2021-04-02 00:34:05 +0300

    Update static_matrix.c

--- tests/time_check/static_time/static_matrix.c
+++ tests/time_check/static_time/static_matrix.c
@@ -7,9 +7,9 @@
 #include <stdio.h>
 
 
-const int test_count = 5;
-int test_vert = 5;
-int test_hor = 10;
+const int test_count = 20;
+int test_vert = 50;
+int test_hor = 100;
 
 const char *test_filename = "test_matrix.txt";
 const char *test_final_filename = "test_final_matrix.txt";
@@ -33,6 +33,6 @@ int main() {
     clock_t end = clock();
 
     double time_spent = (double) (end - begin) / CLOCKS_PER_SEC;
-    printf("Time to mirror static matrix in seconds: %f\n", time_spent);
+    printf("\nTime to mirror static matrix in seconds: %f\n", time_spent);
     return 0;
 }
